25753a14a5548b798818fb672410024c2840c7c2
RodrigoPaulaLopes <rodrigolopesti1@gmail.com>
2021-03-11 16:22:10 -0300

    site SPA com html, css e js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const menuitens = document.querySelectorAll('.menu a')
+
+menuitens.forEach(item =>{
+    item.addEventListener('click', mouseRolar)
+})
+
+function mouseRolar(event){
+    event.preventDefault()
+    const element = event.target
+    const id = element.getAttribute('href')
+    const to = document.querySelector(id).offsetTop
+
+    window.scroll({
+        top: to - 100,
+        behavior: "smooth"
+    })
+
+}
+
